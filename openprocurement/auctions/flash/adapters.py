9e7f8034a7340c4a3087a57fa413d4f11c053bd4
MotsoVitalii <vitalii.motso@gmail.com>
2017-12-21 12:56:28 +0200

    add adapter

---
+++ openprocurement/auctions/flash/adapters.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from openprocurement.auctions.core.adapters import AuctionConfigurator
+
+
+class AuctionFlashConfigurator(AuctionConfigurator):
+    name = 'Auction Flash Configurator'
---
+++ openprocurement/auctions/flash/includeme.py
@@ -0,0 +1,12 @@
+from pyramid.interfaces import IRequest
+from openprocurement.auctions.flash.models import Auction
+from openprocurement.auctions.core.interfaces import IContentConfigurator
+from openprocurement.auctions.core.models import IAuction
+from openprocurement.auctions.flash.adapters import AuctionFlashConfigurator
+
+
+def includeme(config):
+    config.add_auction_procurementMethodType(Auction)
+    config.scan("openprocurement.auctions.flash.views")
+    config.registry.registerAdapter(AuctionFlashConfigurator, (IAuction, IRequest),
+                                    IContentConfigurator)
