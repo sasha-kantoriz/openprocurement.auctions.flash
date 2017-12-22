23b941ca760da9f7fd798c8fbb0319b614e629b0
Oleksandr Vinnichuk <oleksandr.v@quintagroup.org>
2017-12-22 17:53:50 +0200

    Awards refactoring

--- openprocurement/auctions/flash/views/award_complaint.py
+++ openprocurement/auctions/flash/views/award_complaint.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from openprocurement.auctions.core.utils import opresource
-from openprocurement.auctions.core.awarding_1_0.views import (
+from openprocurement.auctions.core.plugins.awarding_1_0.views import (
     AuctionAwardComplaintResource as BaseAuctionAwardComplaintResource
 )
 
