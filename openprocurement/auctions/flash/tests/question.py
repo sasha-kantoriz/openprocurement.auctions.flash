2f243a83103553ad966c6d630547d8577b8a5a4a
Scandie <grizly.vl@gmail.com>
2017-12-29 18:26:20 +0200

    Remove question_blanks.py module
    - Add AuctionQuestionResourceTestMixin

--- openprocurement/auctions/flash/tests/question.py
+++ openprocurement/auctions/flash/tests/question.py
@@ -2,33 +2,25 @@
 import unittest
 
 from openprocurement.auctions.core.tests.base import snitch
-
-from openprocurement.auctions.flash.tests.base import BaseAuctionWebTest, test_lots
-from openprocurement.auctions.flash.tests.blanks.question_blanks import (
-    # AuctionQuestionResourceTest
-    create_auction_question_invalid,
-    create_auction_question,
-    patch_auction_question,
-    get_auction_question,
-    get_auction_questions,
+from openprocurement.auctions.core.tests.question import AuctionQuestionResourceTestMixin
+from openprocurement.auctions.core.tests.blanks.question_blanks import (
     # AuctionLotQuestionResourceTest
-    create_auction_lot_question,
-    patch_auction_lot_question
+    create_auction_question_lot,
+    patch_auction_question_lot
 )
 
+from openprocurement.auctions.flash.tests.base import BaseAuctionWebTest, test_lots, test_organization
 
-class AuctionQuestionResourceTest(BaseAuctionWebTest):
-    test_create_auction_question_invalid = snitch(create_auction_question_invalid)
-    test_create_auction_question = snitch(create_auction_question)
-    test_patch_auction_question = snitch(patch_auction_question)
-    test_get_auction_question = snitch(get_auction_question)
-    test_get_auction_questions = snitch(get_auction_questions)
+
+class AuctionQuestionResourceTest(BaseAuctionWebTest, AuctionQuestionResourceTestMixin):
+    initial_organization = test_organization
 
 
 class AuctionLotQuestionResourceTest(BaseAuctionWebTest):
+    initial_organization = test_organization
     initial_lots = 2 * test_lots
-    test_create_auction_lot_question = snitch(create_auction_lot_question)
-    test_patch_auction_lot_question = snitch(patch_auction_lot_question)
+    test_create_auction_question_lot = snitch(create_auction_question_lot)
+    test_patch_auction_question_lot = snitch(patch_auction_question_lot)
 
 
 def suite():
