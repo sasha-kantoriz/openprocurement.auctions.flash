a89d41a19bb51b6e0cffe5f73c8a496ce3fa61df
sasha-kantoriz <sasha-kantoriz@quintagroup.org>
2018-01-03 17:38:19 +0200

    Refactor document tests

--- openprocurement/auctions/flash/tests/document.py
+++ openprocurement/auctions/flash/tests/document.py
@@ -4,14 +4,18 @@ import unittest
 from openprocurement.auctions.core.tests.base import snitch
 
 from openprocurement.auctions.flash.tests.base import BaseAuctionWebTest
+from openprocurement.auctions.core.tests.blanks.document_blanks import (
+    # AuctionDocumentResourceTest
+    not_found,
+    # AuctionDocumentWithDSResourceTest
+    create_auction_document_json_invalid,
+)
 from openprocurement.auctions.flash.tests.blanks.document_blanks import (
     # AuctionDocumentResourceTest
-    auction_document_not_found,
     create_auction_document,
     put_auction_document,
     patch_auction_document,
     # AuctionDocumentWithDSResourceTest
-    create_auction_document_json_invalid,
     create_auction_document_json,
     put_auction_document_json
 )
@@ -19,7 +23,7 @@ from openprocurement.auctions.flash.tests.blanks.document_blanks import (
 
 class AuctionDocumentResourceTest(BaseAuctionWebTest):
     docservice = False
-    test_auction_document_not_found = snitch(auction_document_not_found)
+    test_auction_document_not_found = snitch(not_found)
     test_create_auction_document = snitch(create_auction_document)
     test_put_auction_document = snitch(put_auction_document)
     test_patch_auction_document = snitch(patch_auction_document)
