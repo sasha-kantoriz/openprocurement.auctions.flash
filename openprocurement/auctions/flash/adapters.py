453f1bca8869d4600d20cd788a4b01bae6187668
vitalii-motso <vitalii.motso@gmail.com>
2018-01-09 14:33:52 +0200

    add utils to congigurator (#23)
    * update using add_next_award
    
    * add utils to configurator

--- openprocurement/auctions/flash/adapters.py
+++ openprocurement/auctions/flash/adapters.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 from openprocurement.auctions.core.adapters import AuctionConfigurator
 from openprocurement.auctions.flash.models import Auction
+from openprocurement.auctions.core.plugins.awarding_1_0.utils import add_next_award
+from openprocurement.auctions.core.plugins.awarding_1_0.models import Award
 
 
 class AuctionFlashConfigurator(AuctionConfigurator):
     name = 'Auction Flash Configurator'
     model = Auction
+    award_model = Award
+
+    def add_award(self):
+        return add_next_award(self.request)
