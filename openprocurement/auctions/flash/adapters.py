b2fa7ad3989d6ed49bcebcc95beeef8e2c8149f0
yarsanich <nezn27@gmail.com>
2018-01-22 16:22:56 +0200

    PEP

--- openprocurement/auctions/flash/adapters.py
+++ openprocurement/auctions/flash/adapters.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from openprocurement.auctions.core.adapters import AuctionConfigurator
-from openprocurement.auctions.core.plugins.awarding.v1.adapters import AwardingV1ConfiguratorMixin
+from openprocurement.auctions.core.plugins.awarding.v1.adapters import (
+    AwardingV1ConfiguratorMixin
+)
 from openprocurement.auctions.flash.models import Auction
 
 
-class AuctionFlashConfigurator(AuctionConfigurator, AwardingV1ConfiguratorMixin):
+class AuctionFlashConfigurator(AuctionConfigurator,
+                               AwardingV1ConfiguratorMixin):
     name = 'Auction Flash Configurator'
     model = Auction
