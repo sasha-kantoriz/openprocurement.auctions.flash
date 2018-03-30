0dab3f0d0b434b2c57ae5292480b2ce605b626de
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-03-30 18:43:02 +0300

    Fix imports

--- openprocurement/auctions/flash/tests/blanks/tender_blanks.py
+++ openprocurement/auctions/flash/tests/blanks/tender_blanks.py
@@ -2,7 +2,7 @@
 from copy import deepcopy
 from datetime import timedelta
 
-from openprocurement.api.models import get_now
+from openprocurement.api.models.auction_models.models import get_now
 from openprocurement.api.constants import SANDBOX_MODE
 
 from openprocurement.auctions.flash.models import Auction
